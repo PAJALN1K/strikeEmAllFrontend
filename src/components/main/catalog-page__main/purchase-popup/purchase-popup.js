0d73e79c46dc4858e2a878a37b2b2cf3472ba91d
springguy <p4j4ln1k@gmail.com>
2023-11-20 04:06:28 +0300

    ✨ Make HTML and CSS for purchase-popup

---
+++ src/components/main/catalog-page__main/purchase-popup/purchase-popup.js
@@ -0,0 +1,7 @@
+const popupLinks = document.querySelectorAll('.catalog-item__purchase-button');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll(".lock-padding");
+
+let unlock = true;
+
+const timeout = 800;
